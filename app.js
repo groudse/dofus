04847c42c07800eaedfbeff3fb3c58843c8bf092
axel f <axel.figerou@gmail.com>
2026-01-13 21:32:49 +0100

    a

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express')
+import cors from 'cors';
 const mongoose = require('mongoose')
 const app = express()
 const items_routes = require('./routes/items-routes.js')
 
 require('dotenv').config()
+
+app.use(cors({
+    origin: 'https://imagiro.laboubourse.com',
+    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Accept']
+}));
+
+
+
+
+
+
 
 mongoose.connect(process.env.MONGO_URI)
     .then((result) => console.log('Connecté à MongoDB'))
